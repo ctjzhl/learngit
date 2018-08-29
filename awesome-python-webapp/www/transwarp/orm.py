ee48210e8095f8306206bd0afd3c3bf915008d95
ctjzhl <caotijian@126.com>
2018-08-29 17:27:10 +0800

    modify orm.py

--- awesome-python-webapp/www/transwarp/orm.py
+++ awesome-python-webapp/www/transwarp/orm.py
@@ -94,7 +94,7 @@ _triggers = frozenset(['pre_insert', 'pre_update', 'pre_delete'])
 def _gen_sql(table_name,mappings):
 	pk = None
 	sql = ['-- generating SQL for %s:' % table_name, 'create table `%s` (' % table_name]
-	for f in shorted(mappings.values(),lambda x,y:cmp(x._order,y._order)):
+	for f in sorted(mappings.values(),lambda x,y:cmp(x._order,y._order)):
 		if not hasattr(f,'ddl'):
 			raise StandardError('no ddl in field "%s".' % f)
 		ddl = f.ddl
@@ -106,7 +106,7 @@ def _gen_sql(table_name,mappings):
 	sql.append(');')
 	return '\n'.join(sql)
 
-class ModeMetaclass(type):
+class ModelMetaclass(type):
 	def __new__(cls,name,bases,attrs):
 		if name=='Model':
 			return type.__new__(cls,name,bases,attrs)
@@ -125,8 +125,153 @@ class ModeMetaclass(type):
 					v.name = k
 				logging.info('Found mapping: %s => %s' % (k, v))
 				if v.primary_key:
-					psss
+					if primary_key:
+						raise TypeError('Cannot define more than 1 primary key in class: %s' % name)
+					if v.updatable:
+						logging.warning('NOTE: change primary key to non-updatable.')
+						v.updatable = False
+					if v.nullable:
+						logging.warning('NOTE: change primary key to non-nullable.')
+						v.nullable = False
+					primary_key = v
+				mappings[k] = v
+		if not primary_key:
+			raise TypeError('Primary key not defined in class: %s' % name)
+		for k in mappings.iterkeys():
+			attrs.pop(k)
+		if not '__table__' in attrs:
+			attrs['__table__'] = name.lower()
+		attrs['__mappings__'] = mappings
+		attrs['__primary_key__'] = primary_key
+		attrs['__sql__'] = lambda self: _gen_sql(attrs['__table__'], mappings)
+		for trigger in _triggers:
+			if not trigger in attrs:
+				attrs[trigger] = None
+		return type.__new__(cls,name,bases,attrs)
+			
+class Model(dict):
+	'''
+	>>> class User(Model):
+	...		id = IntegerField(primary_key=True)
+	...		name = StringField()
+	...		email = StringField(updatable=False)
+	...		passwd = StringField(default=lambda: '******')
+	...		last_modified = FloatField()
+	...		def pre_insert(self):
+	...			self.last_modified = time.time()
+	>>> u = User(id=10190, name='Michael', email='orm@db.org')
+	>>> r = u.insert()
+	>>> u.email
+	'orm@db.org'
+	>>> u.passwd
+	'******'
+	>>> u.last_modified > (time.time() - 2)
+	True
+	>>> f = User.get(10190)
+	>>> f.name
+	u'Michael'
+	>>> f.email
+	u'orm@db.org'
+	>>> f.email = 'changed@db.org'
+	>>> r = f.update() # change email but email is non-updatable!
+	>>> len(User.find_all())
+	1
+	>>> g = User.get(10190)
+	>>> g.email
+	u'orm@db.org'
+	>>> r = g.delete()
+	>>> len(db.select('select * from user where id=10190'))
+	0
+	>>> import json
+	>>> print User().__sql__()
+	-- generating SQL for user:
+	create table `user` (
+	  `id` bigint not null,
+	  `name` varchar(255) not null,
+	  `email` varchar(255) not null,
+	  `passwd` varchar(255) not null,
+	  `last_modified` real not null,
+	  primary key(`id`)
+	);
+	'''
+	__metaclass__ = ModelMetaclass
+	
+	def __init__ (self,**kw):
+		super(Model,self).__init__(**kw)
 
+	def __getattr__ (self,key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+		
+	def __setattr__ (self,key,value):
+		self[key] = value
+	
+	@classmethod
+	def get (cls,pk):
+		d = db.select_one('select * from %s where %s=?' % (cls.__table__, cls.__primary_key__.name), pk)
+		return cls(**d) if d else None
+
+	@classmethod
+	def find_first (cls, where, *args):
+		d = db.select_one('select * from %s %s' % (cls.__table__, where), *args)
+		return cls(**d) if d else None
+	
+	@classmethod
+	def find_all (cls, *args):
+		L = db.select('select * from `%s`' % cls.__table__)
+		return [cls(**d) for d in L]
+		
+	@classmethod
+	def find_by (cls, where, *args):
+		L = db.select('select * from `%s` %s' % (cls.__table__, where), *args)
+		return [cls(**d) for d in L]
+
+	@classmethod
+	def count_all (cls):
+		return db.select_int('select count(`%s`) from `%s`' % (cls.__primary_key__.name, cls.__table__))
+
+	@classmethod
+	def  count_by(cls, where, *args):
+		return db.select_int('select count(`%s`) from `%s` %s' % (cls.__primary_key__.name, cls.__table__, where), *args)
+
+	def update (self):
+		self.pre_update and self.pre_update()
+		L = []
+		args = []
+		for k,v in self.__mappings__.iteritems():
+			if v.updatable:
+				if hasattr(self, k):
+					arg = getattr(self,k)
+				else:
+					arg = v.default
+					setattr(self,k,arg)
+				L.append('`%s`=?' % k)
+				args.append(arg)
+		pk = self.__primary_key__.name
+		args.append(getattr(self, pk))
+		db.update('update `%s` set %s where %s=?' % (self.__table__, ','.join(L), pk), *args)
+		return self
+		
+	def delete (self):
+		self.pre_delete and self.pre_delete()
+		pk = self.__primary_key__.name
+		args = (getattr(self, pk), )
+		db.update('delete from `%s` where `%s`=?' % (self.__table__, pk), *args)
+		return self
+	
+	def insert (self):
+		self.pre_insert and self.pre_insert()
+		params = {}
+		for k,v in self.__mappings__.iteritems():
+			if v.insertable:
+				if not hasattr(self,k):
+					setattr(self,k,v.default)
+				params[v.name] = getattr(self,k)
+		db.insert('%s' % self.__table__, **params)
+		return self		
+			
 if __name__=='__main__':
 	logging.basicConfig(level=logging.DEBUG)
 	db.create_engine('root','root','test')
