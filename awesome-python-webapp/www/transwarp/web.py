244d6f0cae7ca9494511c0d6f4f7d96456bcc3a1
ctjzhl <caotijian@126.com>
2018-08-30 17:59:50 +0800

    modify web.py

--- awesome-python-webapp/www/transwarp/web.py
+++ awesome-python-webapp/www/transwarp/web.py
@@ -58,11 +58,43 @@ class Dict(dict):
 	def __setattr__ (self,key,value):
 		self[key] = value
 
+_TIMEDELTA_ZERO = datetime.timedelta(0)
+_RE_TZ = re.compile('^([\+\-])([0-9]{1,2})\:([0-9]{1,2})$')
 
+class UTC(datetime.tzinfo):
+	def __init__ (self,utc):
+		utc = str(utc.strip().upper())
+		mt = _RE_TZ.match(utc)
+		if mt:
+			minus = mt.group(1)=='-'
+			h = int(mt.group(2))
+			m = int(mt.group(3))
+			if minus:
+				h,m = (-h), (-m)
+			self._utcoffset = datetime.timedelta(hours=h, minutes=m)
+			self._tzname = 'UTC%s' % utc
+		else:
+			raise ValueError('bad utc time zone')
 
+	def utcoffset(self, dt):
+		return self._utcoffset
+
+	def dst (self,dt):
+		return _TIMEDELTA_ZERO
+			
+	def tzname (self,dt):
+		return self._tzname
+		
+	def __str__ (self):
+			return 'UTC tzinfo object (%s)' % self._tzname
+	
+	__repr__ = __str__
 
 
 if __name__ == '__main__':
+	
+	tz0 = UTC('+00:00')
+	print tz0.tzname(None)
 
 	logging.basicConfig(level=logging.DEBUG)
 	sys.path.append('.')
