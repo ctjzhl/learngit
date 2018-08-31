12fcc173c5d85e9f6843d144d2606d3af8b47781
ctjzhl <caotijian@126.com>
2018-08-31 11:51:31 +0800

    modify web.py

--- awesome-python-webapp/www/transwarp/web.py
+++ awesome-python-webapp/www/transwarp/web.py
@@ -121,9 +121,241 @@ _RESPONSE_STATUSES = {
 	101:'Switching Protocols',
 	102:'Processing',
 
-	
+	200:'OK',
+	201:'Created',
+	202:'Accepted',
+	203:'Non-Authoritative Information',
+	204:'No Content',
+	205:'Reset Content',
+	206:'Partial Content',
+	207:'Multi Status',
+	226:'IM Used',
 
+	300:'Multiple Choices',
+	301:'Moved Permanently',
+	302:'Found',
+	303:'See Other',
+	304:'Not Modified',
+	305:'Use Proxy',
+	307:'Temporary Redirect',
+
+	400:'Bad Request',
+	401: 'Unauthorized',
+	402: 'Payment Required',
+	403: 'Forbidden',
+	404: 'Not Found',
+	405: 'Method Not Allowed',
+	406: 'Not Acceptable',
+	407: 'Proxy Authentication Required',
+	408: 'Request Timeout',
+	409: 'Conflict',
+	410: 'Gone',
+	411: 'Length Required',
+	412: 'Precondition Failed',
+	413: 'Request Entity Too Large',
+	414: 'Request URI Too Long',
+	415: 'Unsupported Media Type',
+	416: 'Requested Range Not Satisfiable',
+	417: 'Expectation Failed',
+	418: "I'm a teapot",
+	422: 'Unprocessable Entity',
+	423: 'Locked',
+	424: 'Failed Dependency',
+	426: 'Upgrade Required',
+
+	500: 'Internal Server Error',
+	501: 'Not Implemented',
+	502: 'Bad Gateway',
+	503: 'Service Unavailable',
+	504: 'Gateway Timeout',
+	505: 'HTTP Version Not Supported',
+	507: 'Insufficient Storage',
+	510: 'Not Extended',
 }
+
+_RE_RESPONSE_STATUS = re.compile(r'^\d\d\d(\ [\w\ ]+)?$')
+
+_RESPONSE_HEADERS = (
+	'Accept-Ranges',
+	'Age',
+	'Allow',
+	'Cache-Control',
+	'Connection',
+	'Content-Encoding',
+	'Content-Language',
+	'Content-Length',
+	'Content-Location',
+	'Content-MD5',
+	'Content-Disposition',
+	'Content-Range',
+	'Content-Type',
+	'Date',
+	'ETag',
+	'Expires',
+	'Last-Modified',
+	'Link',
+	'Location',
+	'P3P',
+	'Pragma',
+	'Proxy-Authenticate',
+	'Refresh',
+	'Retry-After',
+	'Server',
+	'Set-Cookie',
+	'Strict-Transport-Security',
+	'Trailer',
+	'Transfer-Encoding',
+	'Vary',
+	'Via',
+	'Warning',
+	'WWW-Authenticate',
+	'X-Frame-Options',
+	'X-XSS-Protection',
+	'X-Content-Type-Options',
+	'X-Forwarded-Proto',
+	'X-Powered-By',
+	'X-UA-Compatible',
+)
+
+_RESPONSE_HEADER_DICT = dict(zip(map(lambda x: x.upper(), _RESPONSE_HEADERS), _RESPONSE_HEADERS))
+
+_HEADER_X_POWERED_BY = ('X-Powered-By', 'transwarp/1.0')
+
+class HttpError(Exception):
+	'''
+	>>> e = HttpError(404)
+	>>> e.status
+	'404 Not Found'
+	'''
+	def __init__ (self,code):
+		super(HttpError,self).__init__()
+		self.status = '%d %s' % (code, _RESPONSE_STATUSES[code])
+
+	def header (self, name, value):
+		if not hasattr(self, '_headers'):
+			self._headers = [_HEADER_X_POWERED_BY]
+		self._headers.append((name,value))
+
+	@property
+	def headers (self):
+		if hasattr(self, '_headers'):
+			return self._headers
+		return []
+
+	def __str__ (self):
+		return self.status
+
+	__repr__ = __str__
+
+class RedirectError(HttpError):
+	
+	'''
+	>>> e = RedirectError(302, 'http://www.apple.com/')
+	>>> e.status
+	'302 Found'
+	>>> e.location
+	'http://www.apple.com/'
+	'''
+	def __init__ (self,code,location):
+		super(RedirectError,self).__init__(code)
+		self.location = location
+
+	def __str__ (self):
+		return '%s, %s' % (self.status, self.location)
+
+	__repr__ = __str__
+
+def badrequest ():
+	'''
+	>>> raise badrequest()
+	Traceback (most recent call last):
+		...
+	HttpError: 400 Bad Request
+	'''
+	return HttpError(400)
+
+def unauthorized ():
+	'''
+	>>> raise unauthorized()
+	Traceback (most recent call last):
+		...
+	HttpError: 401 Unauthorized
+	'''
+	return HttpError(401)
+
+def forbidden ():
+	'''
+	>>> raise forbidden()
+	Traceback (most recent call last):
+		...
+	HttpError: 403 Forbidden
+	'''
+	return HttpError(403)
+
+def notfound ():
+	'''
+	>>> raise notfound()
+	Traceback (most recent call last):
+		...
+	HttpError: 404 Not Found
+	'''
+	return HttpError(404)
+
+def conflict ():
+	'''
+	>>> raise conflict()
+	Traceback (most recent call last):
+		...
+	HttpError: 409 Conflict
+	'''
+	return HttpError(409)
+
+def  internalerror():
+	'''
+	>>> raise internalerror()
+	Traceback (most recent call last):
+		...
+	HttpError: 500 Internal Server Error
+	'''
+	return HttpError(500)
+
+def  redirect(location):
+	'''
+	>>> raise redirect('http://www.itranswarp.com/')
+	Traceback (most recent call last):
+		...
+	RedirectError: 301 Moved Permanently, http://www.itranswarp.com/
+	'''
+	return RedirectError(301,location)
+
+def  found(location):
+	'''
+	>>> raise found('http://www.itranswarp.com/')
+	Traceback (most recent call last):
+		...
+	RedirectError: 302 Found, http://www.itranswarp.com/
+	'''
+	return RedirectError(302,location)
+
+def  seeother(location):
+	'''
+	>>> raise seeother('http://www.itranswarp.com/')
+	Traceback (most recent call last):
+		...
+	RedirectError: 303 See Other, http://www.itranswarp.com/
+	>>> e = seeother('http://www.itranswarp.com/seeother?r=123')
+	>>> e.location
+	'http://www.itranswarp.com/seeother?r=123'
+	'''
+	return RedirectError(303,location)
+
+def _to_str (s):
+	
+	if isintance(s,str):
+		return s
+	if isinstance(s, unicode):
+		return s.encode('utf-8')
+	return str(s)
 
 if __name__ == '__main__':
 	
